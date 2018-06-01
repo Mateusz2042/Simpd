1ddd8c8471e62061a067cbe344ae0064577d2619
Mateusz2042 <you@example.com>
2018-06-01 20:53:52 +0200

    Lab10

---
+++ Lab10/Simpd_Lab10(scatterplot).R
@@ -0,0 +1,16 @@
+library("ggplot2")
+
+df <- as.data.frame(matrix(c(100.25600, 100.25600, 160.51200, 100.25600, 200.25600, 100.25600, 200.25600, 200.25600, 100.51200, 100.25600, 200.25600, 100.25600,
+                             1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0,
+                             24.3, 16.3, 10.2, 24.3, 16.0, 24.3, 16.0, 16.0, 24.2, 16.84, 16.0, 24.3,
+                             2499, 1739, 10199, 1599, 2499, 4999, 3849, 2449, 4399, 1699, 2499, 3495), 12, 4))
+labels <- c("1", "2", "3", "4","5")
+result <- c(1, 4, 5, 4, 3, 4, 1, 2, 3, 2, 3, 2)
+factors <- factor(result, labels)
+df <- cbind(df, factors)
+colnames(df) <- c("ZakresCzu³oœci", "Obiektyw", "Rozdzielczoœæ", "Cena", "Rank")
+
+scatterplot <- ggplot(data=df, aes(x = ZakresCzu³oœci, y = Cena, col = Rank)) + geom_jitter(position = position_jitter(width = 0.2), size=12)
+scatterplot <- scatterplot + geom_point(size = 5) + xlab("ZakresCzu³oœci")+ylab("Cena") + labs(colour = "Rank") + ggtitle("Ranking aparatów zale¿ny od ceny i zakresu czu³oœci")
+scatterplot <- scatterplot + scale_colour_brewer(palette = "Set1") + theme(plot.title = element_text(vjust =+ 2)) + scale_x_continuous(breaks = 1:8)
+scatterplot
---
+++ Lab10/Simpd_Lab10(stripchart).R
@@ -0,0 +1,16 @@
+library("ggplot2")
+
+df <- as.data.frame(matrix(c(100.25600, 100.25600, 160.51200, 100.25600, 200.25600, 100.25600, 200.25600, 200.25600, 100.51200, 100.25600, 200.25600, 100.25600,
+                             1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0,
+                             24.3, 16.3, 10.2, 24.3, 16.0, 24.3, 16.0, 16.0, 24.2, 16.84, 16.0, 24.3,
+                             2499, 1739, 10199, 1599, 2499, 4999, 3849, 2449, 4399, 1699, 2499, 3495), 12, 4))
+labels <- c("1", "2", "3", "4","5")
+result <- c(1, 4, 5, 4, 3, 4, 1, 2, 3, 2, 3, 2)
+factors <- factor(result, labels)
+df <- cbind(df, factors)
+colnames(df) <- c("ZakresCzu³oœci", "Obiektyw", "Rozdzielczoœæ", "Cena", "Rank")
+
+stripchart <- ggplot(df, aes(Rozdzielczoœæ, Cena, col = factor(Rank))) + geom_jitter(position = position_jitter(width = 0.2), size=12)
+stripchart <- stripchart + ylab("Cena") + xlab("Width") + labs(colour = "Rank")
+stripchart <- stripchart + ggtitle("Ceny aparatów i Ranking") + theme(plot.title = element_text(vjust =+ 2))
+stripchart
